133b7a4543dead246c809a5973b3f6be36245f72
joanpong <joanpong@users.noreply.github.com>
2022-08-19 12:06:28 +0800

    committing!

--- App/frontend/src/components/Collapsible/Collapsible.js
+++ App/frontend/src/components/Collapsible/Collapsible.js
@@ -9,18 +9,11 @@ const Collapsible = (props) => {
     <div className="collapsible">
       <div className={open ? "content-show" : "content-parent"}>
         <button className="collapse-button" onClick={toggle}>
-<<<<<<< HEAD
-          {props.label.Country} Total Market Value ({props.label.Country}):
-          {props.label.PortfolioMarketValueTotal}
-          Total Daily P&L: {props.label.PortfolioDailyPnLTotal}
-          Total Unrealised P&L: {props.label.PortfolioUnrealisedPnLTotal}
-=======
           <p>
           {props.label.Country} Total Market Value ({props.label.Country}): {props.label.PortfolioMarketValueTotal}
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Total Daily P&L: {props.label.PortfolioDailyPnLTotal}
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Total Unrealised P&L: {props.label.PortfolioUnrealisedPnLTotal}
           </p>
->>>>>>> origin/janel-fe
         </button>
 
         <div>
@@ -29,13 +22,8 @@ const Collapsible = (props) => {
               <table>
                 <tr>
                   <th>Name/Ticker Symbol</th>
-<<<<<<< HEAD
-                  <th>MV/Qty</th>
-                  <th>Price/Cost</th>
-=======
                   <th>Price/Qty</th>
                   <th>Cost</th>
->>>>>>> origin/janel-fe
                   <th>Daily Profit and Loss</th>
                   <th>Unrealised Profit and Loss</th>
                 </tr>
--- App/frontend/src/pages/Portfolio/portfolio.js
+++ App/frontend/src/pages/Portfolio/portfolio.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect } from "react";
-import api from "../api";
-import Navbar from "../../components/Navbar";
-import Collapsible from "../../components/Collapsible/Collapsible2";
-
-var SGList = new Array();
-var SGtable = new Array();
-var USList = new Array()
-var UStable = new Array();
-
-/********************************** SG TABLE **********************************/
-fetch("http://127.0.0.1:5000/api/portfolio")
-=======
 import React, { useState, useCallback } from "react";
 import api from "../api";
 import Navbar from "../../components/Navbar";
@@ -25,50 +11,16 @@ var USList = new Array();
 var UStable = new Array();
 
 /********************************** SG TABLE **********************************/
-
-// function getSG() {
-// fetch("http://linuxapacgtcb46.conygre.com:8081/api/portfolio")
-//  .then(response => {response.json(); })
-//  .then((data) => {
-//     data.Portfolio.forEach((item, index) => {
-    
-//           var SGlist= "";
-//           if (item.Country == "SGD") {
-         
-//             SGlist+="<table>"
-//             SGlist += "<tr>";
-//             SGlist += "<td>"+item.Name+"</td>";
-//             SGlist+= "<td>" + item.MarketValue + "/" + item.Quantity + "</td>";
-//             SGlist+=  "<td>" + item.Price + "</td>";
-//             SGlist+= "<td>" + item.DailyPnL + "</td>";
-//             SGlist+="<td>" + item.UnrealisedPnL + "</td>";
-//             SGlist += "</tr>";
-//             SGlist+= "</table>"
-        
-//       }
-//       document.getElementById("tbodyitems").innerHTML = SGlist;
-//     });
-    
-//   })
-//   .catch((error) => console.error(error));
-// }
-
  fetch("http://linuxapacgtcb46.conygre.com:8081/api/portfolio")
->>>>>>> origin/janel-fe
   .then((response) => response.json()) // one extra step
   .then((data) => {
-    /**aggregate data here**/
 
     data.Portfolio.forEach((item, index) => {
       if (item.Country == "SGD") {
         SGList.push(
           <table>
             <tr>
-<<<<<<< HEAD
-              <td>{item.Name}</td>
-=======
               <td>{item.Name} / {item.Ticker} </td>
->>>>>>> origin/janel-fe
               <td>
                 {item.MarketValue} / {item.Quantity}
               </td>
@@ -83,11 +35,7 @@ var UStable = new Array();
   })
   .catch((error) => console.error(error));
 
-<<<<<<< HEAD
-fetch("http://127.0.0.1:5000/api/portfolio/total/SGD")
-=======
 fetch("http://linuxapacgtcb46.conygre.com:8081/api/portfolio/total/SGD")
->>>>>>> origin/janel-fe
   .then((response) => response.json()) // one extra step
   .then((data) => {
     /**aggregate data here**/
@@ -98,33 +46,19 @@ fetch("http://linuxapacgtcb46.conygre.com:8081/api/portfolio/total/SGD")
     );
   })
   .catch((error) => console.error(error));
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/janel-fe
 /********************************** SG TABLE **********************************/
 
 /********************************** US TABLE **********************************/
-
-<<<<<<< HEAD
-fetch("http://127.0.0.1:5000/api/portfolio")
-=======
  fetch("http://linuxapacgtcb46.conygre.com:8081/api/portfolio")
->>>>>>> origin/janel-fe
   .then((response) => response.json()) // one extra step
   .then((data) => {
     /**aggregate data here**/
-
     data.Portfolio.forEach((item, index) => {
       if (item.Country == "USD") {
         USList.push(
           <table>
             <tr>
-<<<<<<< HEAD
-              <td>{item.Name}</td>
-=======
               <td>{item.Name} / {item.Ticker}</td>
->>>>>>> origin/janel-fe
               <td>
                 {item.MarketValue} / {item.Quantity}
               </td>
@@ -139,11 +73,7 @@ fetch("http://127.0.0.1:5000/api/portfolio")
   })
   .catch((error) => console.error(error));
 
-<<<<<<< HEAD
-fetch("http://127.0.0.1:5000/api/portfolio/total/USD")
-=======
 fetch("http://linuxapacgtcb46.conygre.com:8081/api/portfolio/total/USD")
->>>>>>> origin/janel-fe
   .then((response) => response.json()) // one extra step
   .then((data) => {
     /**aggregate data here**/
@@ -154,22 +84,6 @@ fetch("http://linuxapacgtcb46.conygre.com:8081/api/portfolio/total/USD")
     );
   })
   .catch((error) => console.error(error));
-
-<<<<<<< HEAD
-  /********************************** US TABLE **********************************/
-
-const Portfolio = () => {
-  return (
-    <div className="Portfolio">
-      <Navbar />
-      {/* <Collapsible/> */}
-      {/* <ul>{countryList}</ul> */}
-      <table>
-        {SGtable}
-        {UStable}
-        </table>
-    </div>
-=======
 /********************************** US TABLE **********************************/
 
 const Portfolio = () => {
@@ -182,8 +96,6 @@ const Portfolio = () => {
       <hr></hr>
       <table>{UStable}</table>
     </div>
-    
->>>>>>> origin/janel-fe
   );
 };
 
